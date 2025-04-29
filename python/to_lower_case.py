52d687c4f2fd02f814194325df7c126775c182fc
yankkvx <yankkvx@gmail.com>
2025-04-29 23:46:14 +0300

    feat: add solution for To Lower Case problem

---
+++ python/to_lower_case.py
@@ -0,0 +1,7 @@
+# 709. To Lower Case (https://leetcode.com/problems/to-lower-case/)
+# Given a string, return the string after converting all uppercase letters to lowercase.
+# Complexity: O(n) time, O(n) space
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
